ee68276b14bb337795ccd2cdb3b0c68d5fa91d27
Your Name <you@example.com>
2023-05-05 21:09:58 +0600

    add env file

--- src/Components/Firebase/Firebase.config.js
+++ src/Components/Firebase/Firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBs3W42KF7j6qs52gl8OMrxdlyThTLTCpI",
-  authDomain: "bd-food-area.firebaseapp.com",
-  projectId: "bd-food-area",
-  storageBucket: "bd-food-area.appspot.com",
-  messagingSenderId: "925257379980",
-  appId: "1:925257379980:web:25e0a75b8e2f6b8154cba9"
+  apiKey: import.meta.env.VITE_APIKEY ,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN ,
+  projectId: import.meta.env.VITE_PROJECTID ,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET ,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID ,
+  appId: import.meta.env.VITE_APPID 
 };
 
 // Initialize Firebase
--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Col, Container, Row } from 'react-bootstrap';
+import { Card, Col, Container, Row } from 'react-bootstrap';
 import ChefData from '../../../Components/ChefData/ChefData';
 
 const Home = () => {
@@ -24,7 +24,73 @@ const Home = () => {
                 </Col>
                 </Row>
             </Container>
+                 
+                 <div>
                  <ChefData/>
+                 </div>
+                 <div><h2 className='text-parimary text-center m-2'> Contact Information</h2></div>
+                 <Container className=' card Bg-primary mt-4 mb-4'>
+                    
+                 <Container>
+                             
+                              <Row>
+                                <Col sm><Col sm>
+                                <Col className='mt-4' md={4}>
+                                <Card style={{ width: '18rem' }}>
+                                    <Card.Img variant="top" height={240} src={`https://i.ibb.co/gzmJ7Rs/chef-1.jpg`} />
+                                    <Card.Body className='bg-light'>
+                                        <Card.Title className='text-danger fw-bold '> Chef Ahmed Hossain </Card.Title>
+
+                                        <div>
+                                            <p className='text-dark'> Dhaka,Mirpur-12 </p>
+                                            <p className='text-dark'>Email:12ygug@gmail.com</p>
+                                            <p className='text-dark'>Phone:0198576856 </p>
+                                        </div>
+
+                                        <button className='bg-primary'>About</button>
+                                    </Card.Body>
+                                </Card>
+                            </Col>
+                        </Col></Col>
+                                <Col sm><Col sm>
+                                <Col className='mt-4' md={4}>
+                                <Card style={{ width: '18rem' }}>
+                                    <Card.Img variant="top" height={240} src={`https://i.ibb.co/RzgbrpV/chef-2.jpg`} />
+                                    <Card.Body className='bg-light'>
+                                        <Card.Title className='text-danger fw-bold '>Chef Fakruddin </Card.Title>
+
+                                        <div>
+                                            <p className='text-dark'> Dhaka,Mirpur-12 </p>
+                                            <p className='text-dark'>Email:12ygug@gmail.com</p>
+                                            <p className='text-dark'>Phone:0198576856 </p>
+                                        </div>
+
+                                        <button className='bg-primary'>About</button>
+                                    </Card.Body>
+                                </Card>
+                            </Col>
+                        </Col></Col>
+                                <Col sm><Col sm>
+                                <Col className='mt-4' md={4}>
+                                <Card style={{ width: '18rem' }}>
+                                    <Card.Img variant="top" height={240} src={`https://i.ibb.co/BVkytH3/chef3.jpg`} />
+                                    <Card.Body className='bg-light'>
+                                        <Card.Title className='text-danger fw-bold '> Manik Miah </Card.Title>
+
+                                        <div>
+                                            <p className='text-dark'> Dhaka,Mirpur-12 </p>
+                                            <p className='text-dark'>Email:12ygug@gmail.com</p>
+                                            <p className='text-dark'>Phone:0198576856 </p>
+                                        </div>
+
+                                          <button className='bg-primary'>About</button>
+                                    </Card.Body>
+                                </Card>
+                            </Col>
+                        </Col></Col>
+                              </Row>
+                            </Container>
+                 </Container>
         </div>
         
     );
